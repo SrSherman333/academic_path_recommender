7032058f58e3a126b26216383d19f8d430f3b42c
SrSherman333 <misatandazobrito12@gmail.com>
2026-01-15 19:00:43 -0500

    create modular structure and the matriz of datas

---
+++ src/core/data_manager.py
@@ -0,0 +1,11 @@
+# Matrix containing data on the hours used to perform activities (columns) in a week (rows)
+WEEKLY_LOG = [
+    [1.0, 1.0, 0.5, 0.5],
+    [0.8, 1.2, 0.7, 0.4],
+    [0.6, 1.4, 0.8, 0.5],
+    [0.5, 1.0, 1.2, 0.3],
+    [0.9, 1.3, 0.7, 0.6],
+    [0.7, 1.1, 0.9, 0.4],
+    [0.6, 1.0, 1.1, 0.5]
+]
+
